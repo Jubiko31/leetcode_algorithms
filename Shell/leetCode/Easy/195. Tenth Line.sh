c7f016100bc9786a1b277d83a87744bc5f7fe8a9
Jubiko31 <jubiko.macaberidze1@gmail.com>
2023-05-02 21:49:17 +0400

    leetcode: 195

---
+++ Shell/leetCode/Easy/195. Tenth Line.sh
@@ -0,0 +1,15 @@
+# ===========Tenth Line================
+#
+# ? Given a text file file.txt, print just the 10th line of the file.
+# TODO Your script should output the tenth line, which is:
+# ```Line 10```
+#
+
+#!/bin/bash
+OUTPUT=$(sed "10q;d" file.txt)
+if [[ -n "$OUTPUT" ]]
+then
+    echo "$OUTPUT"
+else
+    echo "Line not found."
+fi
